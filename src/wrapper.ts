65a76fde3b924b7113bf740b323ae74db7f6cb6c
albertov19 <alberto@purestake.com>
2021-07-15 12:25:49 +0200

    Changes for Moonbase v8

--- src/wrapper.ts
+++ src/wrapper.ts
@@ -30,7 +30,7 @@ export class SOR {
     MULTIADDR: { [chainId: number]: string } = {
         1: '0x514053acec7177e277b947b1ebb5c08ab4c4580e',
         42: '0x71c7f1086aFca7Aa1B0D4d73cfa77979d10D3210',
-        1287: '0xB67390E1b0342992ec03870E936CCb3f1c894C78'
+        1287: '0x37Adf0E4444CcBeAc535A61C790E5aB382A67379'
     };
 
     constructor(
